5dbdd9de95778b38ece94f7e03f8611a9c5c45e9
polarbao <1109096063@qq.com>
2026-06-04 02:03:37 +0800

    添加 slicer_cli 初始入口

---
+++ apps/slicer_cli/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "slice_soft_demo: slicer_cli scaffold\n";
+    std::cout << "下一步：实现 slice_config.json 读取。\n";
+    return 0;
+}
