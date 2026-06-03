5eb6b1921fe3a3524e5ebc171b542773dcdd3c91
polarbao <1109096063@qq.com>
2026-06-04 02:04:01 +0800

    添加 rip_reader_test 初始入口

---
+++ apps/rip_reader_test/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "slice_soft_demo: rip_reader_test scaffold\n";
+    std::cout << "下一步：实现 manifest 和 TIFF 校验。\n";
+    return 0;
+}
